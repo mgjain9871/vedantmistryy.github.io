d0b8a61cd7ad4652d9f9ab7903b4ba2596abe89f
Shubham Dalvi <shubham.dalvi008@gmail.com>
2022-10-02 11:59:14 +0530

    This fixes issue #6

--- Feature Cars/script.js
+++ Feature Cars/script.js
@@ -1,22 +1,19 @@
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".navbar");
 
-let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.navbar');
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
 
-menu.onclick = () =>{
-menu.classList.toggle('fa-times');
-navbar.classList.toggle('active');
-}
+document.querySelector("#login-btn").onclick = () => {
+  document.querySelector(".login-form-container").classList.toggle("active");
+};
 
-document.querySelector('#login-btn').onclick = () =>{
-document.querySelector('.login-form-container').classList.toggle('active');
-}
+document.querySelector("#close-login-form").onclick = () => {
+  document.querySelector(".login-form-container").classList.remove("active");
+};
 
-document.querySelector('#close-login-form').onclick = () =>{
-document.querySelector('.login-form-container').classList.remove('active');
-}
-
-
-
-
-
-
+document.querySelector(".login-form-bg").onclick = () => {
+  document.querySelector(".login-form-container").classList.remove("active");
+};
